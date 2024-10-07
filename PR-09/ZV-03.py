55959bfef73df1911ed55d7519087a471a139138
nikita <67660240+zvshkin@users.noreply.github.com>
2024-10-07 18:21:12 +0300

    Add files via upload

---
+++ PR-09/ZV-03.py
@@ -0,0 +1,2 @@
+summa = sum([z ** 2 for z in range(1,7)])
+print("Сумма квадратов элементов множества:", summa)
---
+++ PR-09/ZV-04.py
@@ -0,0 +1,6 @@
+fruits = {'apple', 'banana', 'cherry', 'avocado', 'pineapple', 'apricot', 'banana', 'avocado', 'grapefruit'}
+
+sorted_fruits = sorted(fruits, reverse=True)
+
+for fruit in sorted_fruits:
+    print(fruit)
